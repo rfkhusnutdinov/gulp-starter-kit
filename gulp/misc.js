64946f5d553823447f49e236d33ee317c1c96e78
Rinat Khusnutdinov <rfkhusnutdinov@yandex.ru>
2024-12-29 17:19:53 +0400

    Изменение стиля написания SCSS

--- gulp/misc.js
+++ gulp/misc.js
@@ -1,6 +1,6 @@
 export const miscTask = () => {
   return app.gulp
-    .src(app.paths.src.misc)
+    .src(app.paths.src.misc, { encoding: false })
     .pipe(app.gulp.dest(app.paths.dist.misc))
     .pipe(app.plugins.browserSync.stream());
 };
