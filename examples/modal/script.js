3c8b4c14d4ad7d88438cc6e20966d450739fe86c
Rinat Khusnutdinov <rfkhusnutdinov@yandex.ru>
2025-07-05 13:59:22 +0400

    Put examples in a sepate folder

---
+++ examples/modal/script.js
@@ -0,0 +1,50 @@
+const closeModal = function (modal) {
+  if (modal instanceof Element) {
+    modal?.close();
+  } else if (typeof modal === "string") {
+    document.querySelector(modal)?.close();
+  } else {
+    console.error(
+      "closeModal: Property `modal` must be HTMLElement or string!"
+    );
+  }
+};
+
+const closeOpenModal = function () {
+  closeModal(document.querySelector("dialog[open]"));
+};
+
+const openModal = function (modal) {
+  if (modal instanceof Element) {
+    modal.showModal();
+  } else if (typeof modal === "string") {
+    document.querySelector(modal)?.showModal();
+  } else {
+    console.error("openModal: Property `modal` must be HTMLElement or string!");
+  }
+};
+
+const openModalButtons = document.querySelectorAll("[data-modal]");
+
+[...openModalButtons].forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const modalSelector = button.getAttribute("data-modal");
+    openModal(modalSelector);
+  });
+});
+
+const modals = document.querySelectorAll(".modal");
+
+[...modals].forEach((modal) => {
+  modal.addEventListener("click", (e) => {
+    closeModal(modal);
+  });
+
+  const modalContent = modal.querySelector(".modal__content");
+
+  modalContent.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+});
