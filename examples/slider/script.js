640cb50e25323ee99a1cc6ad229d5da3f8b59dfe
Rinat Khusnutdinov <rfkhusnutdinov@yandex.ru>
2025-09-24 20:33:10 +0400

    Update examples

--- examples/slider/script.js
+++ examples/slider/script.js
@@ -1,12 +1,12 @@
+import "@splidejs/splide/css/core";
+import Splide from "@splidejs/splide";
+globalThis.Splide = Splide;
+
 /**
  * Sliders
  *
  * Documentation: https://splidejs.com/
  */
-
-import "@splidejs/splide/css/core";
-import Splide from "@splidejs/splide";
-
 [...document.querySelectorAll(".splide")].forEach((slider) => {
   new Splide(slider).mount();
 });
