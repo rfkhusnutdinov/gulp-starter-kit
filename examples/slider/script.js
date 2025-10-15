ad0d4eab86def798fb8a5c77231980d4fe942dcf
Rinat Khusnutdinov <rfkhusnutdinov@yandex.ru>
2025-10-15 17:32:13 +0400

    Update example for sliders

--- examples/slider/script.js
+++ examples/slider/script.js
@@ -8,10 +8,39 @@ globalThis.Splide = Splide;
  * Documentation: https://splidejs.com/
  */
 [...document.querySelectorAll(".splide")].forEach((slider) => {
-  new Splide(slider, {
-    type: "loop",
+  const splide = new Splide(slider, {
+    type: "loop", // slide, loop, fade
     autoWidth: true,
+    focus: 0,
     perMove: 1,
     omitEnd: true,
-  }).mount();
+    // rewind: true,
+    // rewindByDrag: true,
+    // drag: "free", // true, false, free
+    // autoplay: true,
+    // interval: 1000,
+    // pauseOnHover: true,
+    // pauseOnFocus: true,
+    // i18n: {
+    //   prev: "Previous slide",
+    //   next: "Next slide",
+    //   first: "Go to first slide",
+    //   last: "Go to last slide",
+    //   slideX: "Go to slide %s",
+    //   // ...
+    // },
+  });
+
+  // splide.on("overflow", function (isOverflow) {
+  //   splide.go(0);
+  //   splide.options = {
+  //     arrows: isOverflow,
+  //     pagination: isOverflow,
+  //     drag: isOverflow,
+  //     clones: isOverflow ? undefined : 0,
+  //   };
+  // });
+  // Не забыть добавить splide.refresh() в конце
+
+  splide.mount();
 });
